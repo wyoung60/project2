675141c698400d0fb73fc741fdf043faf9a7df54
Wes Young <wesley.young@du.edu>
2021-01-11 09:45:46 -0700

    Changed models js name (Might need more than one) and added sequelize connection code to connection.js

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+//Dependencies
+const Sequelize = require("sequelize");
+
+//Connection to mySQL
+const sequelize = new Sequelize("database_name", "root", "password", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+});
+
+//Export connection
+module.exports = sequelize;
