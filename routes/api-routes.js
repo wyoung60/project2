d622056ae534f48ee6e31ea313c5b316d91582a9
Wes Young <wesley.young@du.edu>
2021-01-11 11:55:52 -0700

    Added basic pseudocode to js files

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,9 @@
 const Resolution = require("../models/resolution");
 
+//Routes
 module.exports = (app) => {
   app.get("/", (req, res) => {
     Resolution.findAll({}).then((results) => res.json(results));
   });
+  //Post route needed
 };
