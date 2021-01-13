503a384c509b97822256e587ecf4241b649a37a1
Wes Young <wesley.young@du.edu>
2021-01-13 09:47:10 -0700

    Deleted unnecessary code

--- models/resolution.js
+++ models/resolution.js
@@ -1,18 +1,3 @@
-//Dependencies
-// const Sequelize = require("sequelize");
-// //Connection to DB
-// const sequelize = require("../config/connection");
-
-// const sequelize = require("../config/connection");
-
-// Resolution model for DB
-// const Resolution = sequelize.define("resolution", {
-//   title: Sequelize.STRING,
-//   //Add the remaining parameters
-// });
-
-// Resolution.sync();
-
 module.exports = (sequelize, DataTypes) => {
   const Resolution = sequelize.define("resolution", {
     title: DataTypes.STRING,
